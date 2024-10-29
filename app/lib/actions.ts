3ffdbfad5d877009209a2ffdc392b695a5aa1a60
naon708 <naoya_nis@icloud.com>
2024-10-30 01:32:46 +0900

    請求書の作成処理を実装

---
+++ app/lib/actions.ts
@@ -0,0 +1,41 @@
+'use server';
+
+import { z } from 'zod';
+import { sql } from '@vercel/postgres';
+import { revalidatePath } from 'next/cache';
+import { redirect } from 'next/navigation';
+
+const FormSchema = z.object({
+  id: z.string(),
+  customerId: z.string(),
+  amount: z.coerce.number(),
+  status: z.enum(['pending', 'paid']),
+  date: z.string(),
+})
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+  const { customerId, amount, status } = CreateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+
+  /** 浮動小数点の誤差を避けるため
+   * @example
+   * $10.50 を保存する場合
+   * 通常の方法 -> 10.50（浮動小数点数として保存）
+   * セント単位 -> 1050（整数として保存）
+  */
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
+
+  await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+  revalidatePath('/dashboard/invocies');
+  redirect('/dashboard/invoices');
+}
