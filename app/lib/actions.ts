e6f45477c0534ba5c1cee10de163af125d30b27c
naon708 <naoya_nis@icloud.com>
2024-11-02 23:53:28 +0900

    請求書の作成処理にバリデーションを追加

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -7,29 +7,56 @@ import { redirect } from 'next/navigation';
 
 const FormSchema = z.object({
   id: z.string(),
-  customerId: z.string(),
-  amount: z.coerce.number(),
-  status: z.enum(['pending', 'paid']),
+  customerId: z.string({
+    invalid_type_error: 'Please select a customer.....'
+  }),
+  amount: z.coerce
+    .number()
+    .gt(0, { message: 'Please enter an amount greater than $0.....' }),
+  status: z.enum(['pending', 'paid'], {
+    invalid_type_error: 'Please select an invoice status.....',
+  }),
   date: z.string(),
 })
 
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-  const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+  // Zod を使ったバリデーション
+  const validatedFields = CreateInvoice.safeParse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-    /** 浮動小数点の誤差を避けるため
-     * @example
-     * $10.50 を保存する場合
-     * 通常の方法 -> 10.50（浮動小数点数として保存）
-     * セント単位 -> 1050（整数として保存）
-    */
-    const amountInCents = amount * 100;
-    const date = new Date().toISOString().split('T')[0];
+
+  // フォームのバリデーションが失敗していたらエラーを返す
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
+  
+  // insert するデータを用意
+  const { customerId, amount, status } = validatedFields.data
+  /** 浮動小数点の誤差を避けるため
+   * @example
+   * $10.50 を保存する場合
+   * 通常の方法 -> 10.50（浮動小数点数として保存）
+   * セント単位 -> 1050（整数として保存）
+  */
+  const amountInCents = amount * 100;
+  const date = new Date().toISOString().split('T')[0];
   
   try {
     await sql`
@@ -40,6 +67,7 @@ export async function createInvoice(formData: FormData) {
     return { message: 'Database Error: Failed to Create Invoice.' };
   }
 
+  // DBへの insert が成功したら、請求書一覧ページのキャッシュを再検証し一覧ページにリダイレクトする
   revalidatePath('/dashboard/invocies');
   redirect('/dashboard/invoices');
 }
